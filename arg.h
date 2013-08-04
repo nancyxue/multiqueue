13f32a8bea0759c3d5be39d5f186ef856a03e1d7
Nancy Xue <inancy.xue@gmail.com>
2013-08-04 21:54:15 +0800

    Update arg.h

--- arg.h
+++ arg.h
@@ -17,9 +17,9 @@ struct ARG_HEAD
 class CArg
 {
 public:
-	static int PostArg(const char * host, int port, const char * query, const char * data, int len, int timeout);  
-	static bool Post(const char * host, int port, const char * query, const char * data, int len, int timeout, string& response);   
-	static bool Post(const char * host, int port, const char * query, map<string,string>& data, int timeout, string& response);  
+	static int PostArg(const char* host, int port, const char* query, const char* data, int len, int timeout);  
+	static bool Post(const char* host, int port, const char* query, const char* data, int len, int timeout, string& response);   
+	static bool Post(const char* host, int port, const char* query, map<string,string>& data, int timeout, string& response);  
 private:
 	static bool getUrlencodedStr(map<string,string>& data, string & result);
 };
