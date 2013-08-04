ec99903412d5693badae003c49598c1a3f52b3f7
Nancy Xue <inancy.xue@gmail.com>
2013-08-04 20:40:43 +0800

    Create arg.h

---
+++ arg.h
@@ -0,0 +1,27 @@
+#ifndef ARG_H_
+#define ARG_H_
+
+#include <string>
+#include <map>
+using namespace std;
+
+
+struct ARG_HEAD
+{
+        unsigned int uVersion;                                        
+	unsigned int uCmd;                                            
+	unsigned int uCount;                                          
+	unsigned int uLen;                                                                              
+};
+
+class CArg
+{
+public:
+	static int PostArg(const char * host, int port, const char * query, const char * data, int len, int timeout);  
+	static bool Post(const char * host, int port, const char * query, const char * data, int len, int timeout, string& response);   
+	static bool Post(const char * host, int port, const char * query, map<string,string>& data, int timeout, string& response);  
+private:
+	static bool getUrlencodedStr(map<string,string>& data, string & result);
+};
+
+#endif //ARG_H_
